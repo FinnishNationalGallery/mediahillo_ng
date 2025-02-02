8c2c4e9ba397709635f859ef31c8f85bdee663e0
Kari Peiponen <kari.peiponen@fng.fi>
2025-02-02 13:35:01 +0200

    Update sip.py

--- blueprints/sip.py
+++ blueprints/sip.py
@@ -52,6 +52,7 @@ def sip_from_directory():
       contract_id="urn:uuid:abcd1234-abcd-1234-5678-abcd1234abcd",
       creator_name="Sigmund Sipenthusiast",
       creator_type="INDIVIDUAL",
+      create_date =  "2024-02-02T11:06:26+00:00",
       last_mod_date = "2025-02-02T11:06:26+00:00"
    )
    try:
