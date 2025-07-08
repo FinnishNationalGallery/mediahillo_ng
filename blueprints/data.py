5845ddbeabe3391cfcf2e1bc02015aed6699fe01
Kari Peiponen <kari.peiponen@fng.fi>
2025-07-08 14:36:14 +0300

    Update data.py

--- blueprints/data.py
+++ blueprints/data.py
@@ -220,7 +220,7 @@ def data_image_folder_process():
             for entry in sorted(os.listdir(image_folder_path)):
                 full_path = os.path.join(image_folder_path, entry)
                 if os.path.isdir(full_path):
-                    return entry  # Palautetaan vain hakemiston nimi, ei koko polkua
+                    return full_path  # Palautetaan vain hakemiston nimi, ei koko polkua
             return None  # Jos alihakemistoja ei löytynyt
         except Exception as e:
             print(f"Virhe: {e}")
